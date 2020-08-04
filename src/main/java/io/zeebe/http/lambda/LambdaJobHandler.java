f72ca917a5062e361cf4dd0f7ba3b930e072da52
Ankit <ankit@Admins-MacBook-Pro.local>
2020-08-04 22:12:55 +0530

    Made Lambda handler generic to invoke with all zeebe variables

--- src/main/java/io/zeebe/http/lambda/LambdaJobHandler.java
+++ src/main/java/io/zeebe/http/lambda/LambdaJobHandler.java
@@ -6,6 +6,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
+
 import org.springframework.stereotype.Component;
 
 import com.amazonaws.regions.Regions;
@@ -15,6 +16,8 @@ import com.amazonaws.services.lambda.model.AWSLambdaException;
 import com.amazonaws.services.lambda.model.InvocationType;
 import com.amazonaws.services.lambda.model.InvokeRequest;
 import com.amazonaws.services.lambda.model.InvokeResult;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.zeebe.client.api.response.ActivatedJob;
 import io.zeebe.client.api.worker.JobClient;
@@ -27,11 +30,8 @@ import lombok.extern.slf4j.Slf4j;
 public class LambdaJobHandler implements JobHandler {
 
   private AWSLambda lambdaClient;
-  private static final String PARAMETER_LAMBDA_NAME = "lambdaName";
-  private static final String PARAMETER_LAMBDA_INPUT = "lambdaInput";
-  private static final String PARAMETER_LAMBDA_OUTPUT = "lambdaOutput";
-  private static final String PARAMETER_LAMBDA_INPUT_KEYNAME = "lambdaInputKeyName";
-  private static final String PARAMETER_LAMBDA_OUTPUT_KEYNAME = "lambdaOutputKeyName";
+  private final ObjectMapper MAPPER = new ObjectMapper();
+  private final String PARAMETER_LAMBDA_NAME = "lambdaName";
 
   @PostConstruct
   public void init() {
@@ -45,16 +45,10 @@ public class LambdaJobHandler implements JobHandler {
     try {
       Map<String, Object> jobVariables = job.getVariablesAsMap();
       String lambdaName = job.getCustomHeaders().get(PARAMETER_LAMBDA_NAME);
-      String lambdaInputKeyName =
-          job.getCustomHeaders()
-              .getOrDefault(PARAMETER_LAMBDA_INPUT_KEYNAME, PARAMETER_LAMBDA_INPUT);
-      String lambdaOutputKeyName =
-          job.getCustomHeaders()
-              .getOrDefault(PARAMETER_LAMBDA_OUTPUT_KEYNAME, PARAMETER_LAMBDA_OUTPUT);
 
-      String lambdaInput = (String) jobVariables.get(lambdaInputKeyName);
+      String jobVariablesString = MAPPER.writeValueAsString(jobVariables);
       InvokeRequest lambdaRequest =
-          new InvokeRequest().withFunctionName(lambdaName).withPayload(lambdaInput);
+          new InvokeRequest().withFunctionName(lambdaName).withPayload(jobVariablesString);
       lambdaRequest.setInvocationType(InvocationType.RequestResponse); // Use Event for Async
 
       InvokeResult lambdaResult = this.lambdaClient.invoke(lambdaRequest);
@@ -65,12 +59,24 @@ public class LambdaJobHandler implements JobHandler {
       }
       log.info(":::: Output of lambda function {} : {} ::::", lambdaName, responseData);
 
-      jobVariables.put(lambdaOutputKeyName, responseData);
-      client.newCompleteCommand(job.getKey()).variables(jobVariables).send().join();
+      Map<String, Object> outputVariables;
+      if (responseData != null) {
+        outputVariables =
+            MAPPER.readValue(responseData, new TypeReference<Map<String, Object>>() {});
+      } else {
+        outputVariables = jobVariables;
+      }
+      client.newCompleteCommand(job.getKey()).variables(outputVariables).send().join();
+
     } catch (AWSLambdaException e) {
       log.error(
           ":::: Exception occurred while invoking Lambda due to {} ::::", e.getErrorMessage());
-      client.newFailCommand(job.getKey()).retries(0).errorMessage(e.getErrorMessage()).send().join();
+      client
+          .newFailCommand(job.getKey())
+          .retries(0)
+          .errorMessage(e.getErrorMessage())
+          .send()
+          .join();
     }
   }
 }
